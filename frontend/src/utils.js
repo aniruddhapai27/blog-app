3e1dd4980c3e463f9a3c40a4cc2ed332211e305e
aniruddhapai27 <aniruddhaanantpai@gmail.com>
2024-10-27 16:20:39 +0530

    url update

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://blog-app-we8n.onrender.com";
-export const BACKEND_URL = "http://localhost:4001";
+export const BACKEND_URL = "https://blog-app-we8n.onrender.com";
+// export const BACKEND_URL = "http://localhost:4001";
